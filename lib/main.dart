a13ba62c0fd526c11ded8f3a1528203992b0e356
ThankYouJ <parit1544@gmail.com>
2025-03-31 13:02:59 +0700

    remove test

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hotel DD App test',
+      title: 'Hotel DD App',
       theme: ThemeData(
         primarySwatch: Colors.blue, // ธีมสีน้ำเงิน
       ),
